1b8dd9ae5b4530e7a3df7709838c05fd3deea2a4
Renato Duarte <rntdrt@gitlab.com>
2017-06-12 09:28:35 +0100

    spotify-exercise Back end structure with user implementation

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const TOKEN_SECRET = process.env.TOKEN_SECRET || 'spotify-example';
+const SPOTIFY_CLIENT_ID = '4d2612f8c9824845b4a745559b97f05e';
+const SPOTIFY_CLIENT_SECRET = '6386d9c89a254ac2b7741dcdf0b396e8';
+
+export {
+    TOKEN_SECRET,
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET
+};
---
+++ server/index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import path from 'path';
+import volleyball from 'volleyball';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import { signup, login, verifyAuth } from './routes/user';
+import { getAccessToken, getRomanceMusics, } from './routes/spotify';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(volleyball);
+
+app.use(express.static(path.resolve(__dirname, '..', 'client/dist')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('client/dist/index.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const options = {
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+};
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost', options);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.post('/auth/login', login);
+app.post('/auth/signup', signup);
+
+app.get('/musics', getRomanceMusics);
+
+getAccessToken();
+
+app.listen(port, function () {
+    console.log("Rockin' out on port 8080 homie");
+});
